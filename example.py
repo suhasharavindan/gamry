b79652af0b13c3fa88f7528e5784e6335d6f7a50
Suhash Aravindan <suhash.aravindan@gmail.com>
2022-09-05 17:38:19 -0700

    Update example

--- example.py
+++ example.py
@@ -1,6 +1,27 @@
-from gamry.data import load_signals
+from gamry.data import load_signals, filter_signals
 from gamry import plot
 
 signals = load_signals()
 
-plot.eispot_bode(signals, 'Test', 'Sample')
+s1_02mm = filter_signals(signals, label='S1-0.2mm')
+s1_05mm = filter_signals(signals, label='S1-0.5mm')
+s1_1mm = filter_signals(signals, label='S1-1.0mm')
+s2_02mm = filter_signals(signals, label='S2-0.2mm')
+s2_05mm = filter_signals(signals, label='S2-0.5mm')
+s2_1mm = filter_signals(signals, label='S2-1.0mm')
+s5_1mm = filter_signals(signals, label='S5-1.0mm')
+
+names = [
+    'S1-0.2mm',
+    'S1-0.5mm',
+    'S1-1.0mm',
+    'S2-0.2mm',
+    'S2-0.5mm',
+    'S2-1.0mm',
+    'S5-1.0mm',
+]
+
+cnt = 0
+for signals in [s1_02mm, s1_05mm, s1_1mm, s2_02mm, s2_05mm, s2_1mm, s5_1mm]:
+    plot.eispot_bode(signals, names[cnt], 'Samples', theme='plain').show()
+    cnt +=  1
